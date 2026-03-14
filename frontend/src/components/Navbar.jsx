e22f8b64887dae2497e201bfacca071392cd4f8a
patriklie <patrik.lie@hotmail.com>
2026-03-14 22:01:01 +0100

    Hamburger menu for app almost done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { NavLink } from "react-router";
 import { Squash as Hamburger } from 'hamburger-react';
 import { motion, AnimatePresence } from 'motion/react';
 import { useState } from "react";
+import { Info, ClipboardClock } from "lucide-react";
 
 const Navbar = () => {
 
@@ -11,25 +12,38 @@ const Navbar = () => {
     <>  
       <nav className="navbar">
 
-
-      <div className="navbar-header">
-        <div className="navbar-ham"><Hamburger rounded color="#fff" toggled={isOpen} toggle={SetIsOpen} distance='sm' hideOutline={false} /></div>
-        <div className="navbar-logo"><span>Tempus</span>Cura</div>
-      </div>
-
-<AnimatePresence>
-    {isOpen && (
-        <motion.div
-          initial={{ x: "-100%" }}
-          animate={{ x: 0 }}
-          exit={{ x: "-100%"}}
-          transition={{ type: "spring", stiffness: 300, damping: 35 }}
-          className="navbar-sidebar"
+        <motion.div className="navbar-animasjon"
+          animate={{
+            height: isOpen ? "calc(100vh - 32px)" : "60px"
+          }}
+          transition={{ type: "spring", duration: .5 }}
         >
+
+          <div className="navbar-top">
+            <div className="navbar-ham"><Hamburger size={20} rounded color="#000" toggled={isOpen} toggle={SetIsOpen} distance='sm' hideOutline={false} /></div>
+            <div className="navbar-logo"><span>Behandler</span>Booking</div>
+          </div>
+
+          <motion.div 
+            className="navbar-divider"
+            initial={{ scaleX: 0 }}
+            animate={{ scaleX: isOpen ? 1 : 0 }}
+            transition={{ type: "spring", duration: 0.3 }}
+          />
+          
+
+          <div className="navbar-links">
+            <ClipboardClock color="black" size={30} strokeWidth={1.5}/>
+            <NavLink to="/avtaler">Mine Avtaler</NavLink>
+          </div>
+
+          <div className="navbar-links">
+            <Info color="black" size={30} strokeWidth={1.5}/>
+            <NavLink to="/avtaler">Om oss</NavLink>
+          </div>
+    
+
         </motion.div>
-    )}
-</AnimatePresence>
- 
 
 
       </nav>
